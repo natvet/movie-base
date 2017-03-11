33bfad91cd9bb3964419d028463485969a54125d
Natalia Vetter <nataliavetter@gmail.com>
2017-03-11 14:11:19 +0100

    chart draft

--- js/script.js
+++ js/script.js
@@ -9,22 +9,7 @@ $(document).ready(function () {
         });
     }
 
-    function fetchMovies() {
-        var movies;
-        fetch('https://movie-ranking.herokuapp.com/movies')
-            .then((resp) => resp.json())
-            .then(function (data) {
-                movies = data;
-                createList(movies);
-                sortMovies(movies);
-            })
-            .catch(function () {
-                console.log('error');
-            })
-    }
-
     function sortMovies(movies) {
-       
         $('#sortButton').on('click', function (e) {
             $(this).toggleClass('ascending');
             if (!$(this).hasClass('ascending')) {
@@ -50,72 +35,82 @@ $(document).ready(function () {
             createList(movies);
         });
     }
+
+    function fetchRating() {
+        $('.movie-list-item').on('click', function (e) {
+            var id = $(this).attr('data-id');
+            fetch('https://movie-ranking.herokuapp.com/movies/' + id + '/ratings')
+                .then((resp) => resp.json())
+                .then(function (data) {
+                    let ratings = data;
+                    let result = [];
+                    console.log(ratings);
+                    for (var i = 1; i <= 5; i++) {
+                        var filtered = ratings.filter(function (movie) {
+                            return movie.rating === i;
+                        });
+                        var frequency = filtered.length;
+                        result.push(frequency);
+                    }
+                    showRating(result);
+                })
+                .catch(function () {
+                    console.log('error2');
+                })
+        })
+    }
+
+    function showRating(ratings) {
+        var ctx = $("#myChart"); //todo remove old chart
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: [1, 2, 3, 4, 5],
+                datasets: [{
+                    label: '# of Votes',
+                    data: ratings,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    }
+
+    function fetchMovies() {
+        var movies;
+        fetch('https://movie-ranking.herokuapp.com/movies')
+            .then((resp) => resp.json())
+            .then(function (data) {
+                movies = data;
+                createList(movies);
+                sortMovies(movies);
+                fetchRating();
+            })
+            .catch(function () {
+                console.log('error');
+            })
+    }
     fetchMovies();
-
-
-    //         $('.movie-list-item').on('click', function (e) {
-    //             var id = $(this).attr('data-id');
-    //             fetch('https://movie-ranking.herokuapp.com/movies/' + id + '/ratings')
-    //                 .then((resp) => resp.json())
-    //                 .then(function (data) {
-    //                     let ratings = data;
-    //                     let result = [];
-    //                     console.log(ratings);
-    //                     for (var i = 1; i <= 5; i++) {
-    //                         var filtered = ratings.filter(function (movie) {
-    //                             return movie.rating === i;
-    //                         });
-    //                         var frequency = filtered.length;
-    //                         result.push(frequency);
-    //                     }
-    //                     console.log(result);
-    //                     var ctx = $("#myChart"); //todo remove old chart
-    //                     var myChart = new Chart(ctx, {
-    //                         type: 'bar',
-    //                         data: {
-    //                             labels: [1, 2, 3, 4, 5],
-    //                             datasets: [{
-    //                                 label: '# of Votes',
-    //                                 data: result,
-    //                                 backgroundColor: [
-    //                                     'rgba(255, 99, 132, 0.2)',
-    //                                     'rgba(54, 162, 235, 0.2)',
-    //                                     'rgba(255, 206, 86, 0.2)',
-    //                                     'rgba(75, 192, 192, 0.2)',
-    //                                     'rgba(153, 102, 255, 0.2)'
-    //                                 ],
-    //                                 borderColor: [
-    //                                     'rgba(255,99,132,1)',
-    //                                     'rgba(54, 162, 235, 1)',
-    //                                     'rgba(255, 206, 86, 1)',
-    //                                     'rgba(75, 192, 192, 1)',
-    //                                     'rgba(153, 102, 255, 1)'
-    //                                 ],
-    //                                 borderWidth: 1
-    //                             }]
-    //                         },
-    //                         options: {
-    //                             scales: {
-    //                                 yAxes: [{
-    //                                     ticks: {
-    //                                         beginAtZero: true
-    //                                     }
-    //                                 }]
-    //                             }
-    //                         }
-    //                     });
-    //                 })
-    //                 .catch(function () {
-    //                     console.log('error2');
-    //                 });
-    //         })
-    //     })
-    // .catch(function () {
-    //     console.log('error');
-    // });
-
-
-
-
-
 });
